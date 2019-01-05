32f234b819dbeb7b2896b7a8131c419e5f02d611
idhla <34611950+idhla@users.noreply.github.com>
2019-01-06 06:13:18 +1000

    Add project files.

---
+++ RoundedBoxViewRenderer/RoundedBoxViewRenderer.Android/RoundedBoxViewRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RoundedBoxViewRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(RoundedBoxView), typeof(RoundedBoxViewRenderer.Droid.RoundedBoxViewRenderer))]
+
+namespace RoundedBoxViewRenderer.Droid
+{
+    class RoundedBoxViewRenderer : BoxRenderer
+    {
+        public RoundedBoxViewRenderer()
+        {
+            this.SetWillNotDraw(false);
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            RoundedBoxView rbv = (RoundedBoxView) this.Element;
+
+            Rect rc = new Rect();
+            GetDrawingRect(rc);
+
+            Rect interior = rc;
+
+            interior.Inset((int)rbv.StrokeThickness, (int)rbv.StrokeThickness);
+
+            Paint p = new Paint()
+            {
+                Color = rbv.Color.ToAndroid(),
+                AntiAlias = true,
+            };
+
+            canvas.DrawRoundRect(new RectF(interior), (float)rbv.CornerRadius, (float)rbv.CornerRadius, p);
+
+            p.Color = rbv.Stroke.ToAndroid();
+            p.StrokeWidth = (float) rbv.StrokeThickness;
+            p.SetStyle(Paint.Style.Stroke);
+
+            canvas.DrawRoundRect(new RectF(rc), (float)rbv.CornerRadius,  (float)rbv.CornerRadius, p);
+        }
+    }
+}
---
+++ RoundedBoxViewRenderer/RoundedBoxViewRenderer.iOS/RoundedBoxViewRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using RoundedBoxViewRenderer;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRendererAttribute(typeof(RoundedBoxView), typeof(RoundedBoxViewRenderer.iOS.RoundedBoxViewRenderer)) ]
+
+namespace RoundedBoxViewRenderer.iOS
+{
+   public class RoundedBoxViewRenderer : BoxRenderer
+    {
+        public void Draw(System.Drawing.RectangleF rect)
+        {
+            RoundedBoxView rbv = (RoundedBoxView) this.Element;
+
+            using (var context = UIGraphics.GetCurrentContext())
+            {
+                context.SetFillColor(rbv.Color.ToCGColor());
+                context.SetStrokeColor(rbv.Stroke.ToCGColor());
+                context.SetLineWidth((float)rbv.StrokeThickness);
+
+                var rc = this.Bounds.Inset((int)rbv.StrokeThickness, (int)rbv.StrokeThickness);
+
+                float radius = (float) rbv.CornerRadius;
+                radius = (float) Math.Max(0, Math.Min(radius, Math.Max(rc.Height / 2, rc.Width / 2)));
+
+                var path = CGPath.FromRoundedRect(rc, radius, radius);
+                context.AddPath(path);
+                context.DrawPath(CGPathDrawingMode.FillStroke);
+
+
+            }
+        }
+    }
+}
